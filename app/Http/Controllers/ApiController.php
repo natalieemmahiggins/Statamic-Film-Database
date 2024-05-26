0b62b240831d909737daca5124c86a27133f9a39
Nat <nwilliams2094@gmail.com>
2024-05-26 08:57:06 +0100

    make collection and entry foreach api result

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Statamic\Facades\Entry;
 
 class ApiController extends Controller
 {
@@ -15,6 +16,24 @@ class ApiController extends Controller
             'accept' => 'application/json',
         ])->get('https://api.themoviedb.org/3/movie/top_rated');
     
-        dd($response->body());
+        $films = json_decode($response->body())->results;
+            
+        foreach ($films as $film) {
+            //make yaml entry from the api data
+            $slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', preg_replace('/[^\da-z ]/i', '', $film->title) )));
+            $entry = Entry::make()
+            ->published(true)
+            ->collection('films')
+            ->data([
+                'title' => $film->title, 
+                'slug' => $slug,
+                'api_id' => $film->id,
+                'rating' => $film->vote_average,
+                'popularity' => $film->popularity,
+                'overview' => $film->overview,
+                'release_date' => $film->release_date,
+            ]);
+            $entry->save();
+        }
     }
 }
