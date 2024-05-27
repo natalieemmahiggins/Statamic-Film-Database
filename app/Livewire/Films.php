ce22e8cd0e4a40b664a2a9459a97f57a2cea5191
Nat <nwilliams2094@gmail.com>
2024-05-27 10:46:38 +0100

    install livewire, move page content into livewire component

---
+++ app/Livewire/Films.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Films extends Component
+{
+    public function render()
+    {
+        return view('livewire.films');
+    }
+}
